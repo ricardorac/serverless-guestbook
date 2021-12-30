fcff26eb60509c51c06658a993b248aac7143fec
Ricardo Costa <62949206+ricardorac@users.noreply.github.com>
2021-12-30 16:10:58 -0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://e9abf37b.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
